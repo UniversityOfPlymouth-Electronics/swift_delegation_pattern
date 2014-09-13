18c45ac31d2533f280cdea88d5a2fa01b009b490
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2014-09-13 14:07:16 +0100

    Clarified the comments

--- DelegationExample/MyModalViewController.swift
+++ DelegationExample/MyModalViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 Plymouth University. All rights reserved.
 //
 
+// I SUSPECT THIS VERSION TO CREATE A RETAIN CYCLE
+
 import UIKit
 
 protocol MyModalViewControllerProtocol {
@@ -16,6 +18,7 @@ class MyModalViewController: UIViewController {
 
     //The delegate might be a value type, so it cannot be weak or unowned
     //Does this cause a retain cycle?
+    //Semantically it "suggests" ownership
     var delegate : MyModalViewControllerProtocol?
     
     @IBAction func doDismiss(sender: AnyObject) {
