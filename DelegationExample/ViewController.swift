8a71bc0ec5beb3af28ee5aad7aadca46e8a00cad
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2014-09-13 13:30:02 +0100

    Initial Commit

---
+++ DelegationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelegationExample
+//
+//  Created by Nicholas Outram on 13/09/2014.
+//  Copyright (c) 2014 Plymouth University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
