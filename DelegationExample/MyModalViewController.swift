44972ef9086668cf3707a6ee028d12456bb1f570
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2014-09-13 14:26:28 +0100

    A more type-strict delegation pattern. This ensure the presenting object is actually a ViewController AND conforms to the protocol API

--- DelegationExample/MyModalViewController.swift
+++ DelegationExample/MyModalViewController.swift
@@ -10,6 +10,12 @@
 
 import UIKit
 
+//Empty protocol
+protocol MyModalViewControllerIsUIViewController { }
+
+//ONLY UIViewController and all its subclasses implement this protocol
+extension UIViewController : MyModalViewControllerIsUIViewController { }
+
 //I prefer this approach - the protocol is specified to be a class protocol
 //No textual references to ObjectiveC, keeping this clean Swift
 
@@ -20,8 +26,14 @@ protocol MyModalViewControllerProtocol : class {
 class MyModalViewController: UIViewController {
 
     //The delegate must be a reference type (class), so it can now be weak or unowned
-    //Semantically the delegate no longer implies ownership
-    weak var delegate : MyModalViewControllerProtocol?
+
+    // Using a composite protocol, this class implements both the following:
+    
+    //      MyModalViewControllerProtocol           - to ensure the correct methods are implmented
+    //      MyModalViewControllerIsUIViewController - to ensure it is of type UIViewController (or a subclass)
+    
+    // The compiler will only allow the delegate to be set to an object that conforms
+    weak var delegate : protocol<MyModalViewControllerProtocol, MyModalViewControllerIsUIViewController>?
     
     @IBAction func doDismiss(sender: AnyObject) {
         //Call dismiss method on presenting object if the delegate is set
